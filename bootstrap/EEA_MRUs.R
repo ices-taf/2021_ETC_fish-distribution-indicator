17651722a437ba80c275f109bb06c8cab62abdca
Colin Millar <colin.millar@ices.dk>
2022-05-10 14:14:07 +0200

    mode = "wb" for binary files

--- bootstrap/EEA_MRUs.R
+++ bootstrap/EEA_MRUs.R
@@ -1,4 +1,4 @@
-
+library(dplyr)
 
 # change time to 1000 as service can be very slow
 to <- getOption("timeout")
@@ -6,7 +6,8 @@ options(timeout = 10000)
 
 download.file(
   "https://sdi.eea.europa.eu/datashare/s/H46FqeKNacXzJoR/download?path=%2FEuropeSeas_GPKG&files=MSFD_Publication_20181129_EuropeSeas.gpkg&downloadStartSecret=qpdy3m88r9l",
-  "MSFD_Publication_20181129_EuropeSeas.gpkg"
+  "MSFD_Publication_20181129_EuropeSeas.gpkg",
+  mode = "wb"
 )
 
 # reset timeout
